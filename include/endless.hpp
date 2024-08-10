8354dd6a4f888a3c712c1826803d5392554f6bfb
TheZipCreator <thezipcreator@protonmail.com>
2024-08-10 19:28:37 -0400

    added time and score

--- include/endless.hpp
+++ include/endless.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <optional>
+#include <chrono>
 
 #include "GlobalNamespace/BeatmapLevel.hpp"
 #include "GlobalNamespace/BeatmapDifficulty.hpp"
 #include "GlobalNamespace/BeatmapCharacteristicSO.hpp"
+
+#include "HMUI/CurvedTextMeshPro.hpp"
 
 #include "misc.hpp"
 
@@ -13,12 +16,21 @@ namespace endless {
 	struct State {
 		// whether endless mode is currently activated
 		bool activated = false;
+		// time endless mode was started
+		std::chrono::steady_clock::time_point start_time;
+		// total score ammased from levels before the current one
+		int score;
+		// HUD text
+		SafePtrUnity<HMUI::CurvedTextMeshPro> time_text;
+		SafePtrUnity<HMUI::CurvedTextMeshPro> score_text;
 	};
 	extern State state;
 	// starts endless mode
 	void start_endless(void);
 	// starts the next level in endless mode. Returns whether it actually started
 	bool next_level(void);
+	// Updates the score text with the current score on the map currently being played
+	void set_score_text(int score);
 	// Returns the next level, if one can be found
 	std::optional<LevelParams> get_next_level();
 	// registers hooks
--- src/endless.cpp
+++ src/endless.cpp
@@ -11,6 +11,10 @@
 #include "GlobalNamespace/PauseMenuManager.hpp"
 #include "GlobalNamespace/GameScenesManager.hpp"
 #include "GlobalNamespace/LevelBar.hpp"
+#include "GlobalNamespace/ScoreUIController.hpp"
+#include "GlobalNamespace/CoreGameHUDController.hpp"
+#include "GlobalNamespace/ResultsViewController.hpp"
+#include "GlobalNamespace/ScoreFormatter.hpp"
 
 #include "Zenject/DiContainer.hpp"
 
@@ -18,8 +22,14 @@
 #include "UnityEngine/RectTransform.hpp"
 #include "UnityEngine/Canvas.hpp"
 #include "UnityEngine/UI/Button.hpp"
+#include "UnityEngine/WaitForSecondsRealtime.hpp"
+
+#include "TMPro/TextMeshProUGUI.hpp"
+
+#include "HMUI/CurvedTextMeshPro.hpp"
 
 #include "System/Action_1.hpp"
+#include "System/Collections/IEnumerator.hpp"
 
 #include "custom-types/shared/delegate.hpp"
 
@@ -40,8 +50,8 @@ static bool endless_should_continue(GlobalNamespace::LevelCompletionResults *lcr
 		(lcr->levelEndStateType == GlobalNamespace::LevelCompletionResults::LevelEndStateType::Failed && getModConfig().continue_on_fail.GetValue());
 }
 
-MAKE_HOOK_MATCH(pause_start_hook, &GlobalNamespace::PauseMenuManager::Start, void, GlobalNamespace::PauseMenuManager *self) {
-	pause_start_hook(self);
+MAKE_HOOK_MATCH(PauseMenuManager_Start, &GlobalNamespace::PauseMenuManager::Start, void, GlobalNamespace::PauseMenuManager *self) {
+	PauseMenuManager_Start(self);
 	static SafePtrUnity<UnityEngine::UI::Button> button = SafePtrUnity<UnityEngine::UI::Button>();
 	if(!button) {
 		// create skip button
@@ -59,24 +69,128 @@ MAKE_HOOK_MATCH(pause_start_hook, &GlobalNamespace::PauseMenuManager::Start, voi
 	button->gameObject->SetActive(endless::state.activated);
 }
 
-MAKE_HOOK_MATCH(game_finish_hook, &GlobalNamespace::MenuTransitionsHelper::HandleMainGameSceneDidFinish, void, 
+MAKE_HOOK_MATCH(PauseMenuManager_MenuButtonPressed, &GlobalNamespace::PauseMenuManager::MenuButtonPressed, void, GlobalNamespace::PauseMenuManager *self) {
+	PauseMenuManager_MenuButtonPressed(self);
+	endless::state.activated = false;
+}
+
+
+MAKE_HOOK_MATCH(ScoreUIController_Start, &GlobalNamespace::ScoreUIController::Start, void, GlobalNamespace::ScoreUIController *self) {
+	ScoreUIController_Start(self);
+	if(!endless::state.time_text) {
+		auto canvas = UnityEngine::Object::FindObjectOfType<GlobalNamespace::CoreGameHUDController *>()->_energyPanelGO->GetComponentInChildren<UnityEngine::Canvas *>();
+		RETURN_IF_NULL(canvas,);
+		endless::state.time_text.emplace(BSML::Lite::CreateText(canvas->transform, "", TMPro::FontStyles::Normal, 14.f, {-50, 208}, {100, 5}));
+		endless::state.score_text.emplace(BSML::Lite::CreateText(canvas->transform, "", TMPro::FontStyles::Normal, 14.f, {70, 208}, {100, 5}));
+	}
+	endless::state.time_text->gameObject->SetActive(endless::state.activated);
+	endless::state.score_text->gameObject->SetActive(endless::state.activated);
+	if(endless::state.activated)
+		endless::set_score_text(0);
+}
+
+MAKE_HOOK_MATCH(ScoreUIController_UpdateScore, &GlobalNamespace::ScoreUIController::UpdateScore, void,
+	GlobalNamespace::ScoreUIController *self,
+	int multipliedScore,
+	int modifiedScore
+) {
+	ScoreUIController_UpdateScore(self, multipliedScore, modifiedScore);
+	if(!endless::state.activated)
+		return;
+	endless::set_score_text(modifiedScore);
+}
+
+MAKE_HOOK_MATCH(MenuTransitionsHelper_HandleMainGameSceneDidFinish, &GlobalNamespace::MenuTransitionsHelper::HandleMainGameSceneDidFinish, void, 
 	GlobalNamespace::MenuTransitionsHelper *self,
 	GlobalNamespace::StandardLevelScenesTransitionSetupDataSO *slstsdSO,
 	GlobalNamespace::LevelCompletionResults *lcr
 ) {
 	// TODO: this doesn't record scores or anything
 	if(endless::state.activated && endless_should_continue(lcr)) {
+		if(lcr->levelEndStateType == GlobalNamespace::LevelCompletionResults::LevelEndStateType::Cleared)
+			endless::state.score += lcr->modifiedScore;
 		self->_gameScenesManager->PopScenes(0.f, nullptr, custom_types::MakeDelegate<System::Action_1<Zenject::DiContainer*>*>(std::function([self](Zenject::DiContainer *unused) {
 			endless::next_level();
 		})));
 		return;
 	}
-	endless::state.activated = false;
-	game_finish_hook(self, slstsdSO, lcr);
+	// endless::state.activated = false;
+	MenuTransitionsHelper_HandleMainGameSceneDidFinish(self, slstsdSO, lcr);
 }
+
+MAKE_HOOK_MATCH(ResultsViewController_SetDataToUI, &GlobalNamespace::ResultsViewController::SetDataToUI, void, GlobalNamespace::ResultsViewController *self) {
+	
+	// objects to deactivate/reactivate
+	std::vector<UnityEngine::GameObject *> objects = {
+		self->_levelBar->gameObject,
+		self->_newHighScoreText->gameObject
+	};
+	// add things that aren't the score to the list of objects 
+	// This also removes the label that literally says "Score". FIXME
+	{
+		auto child_to_keep = self->_scoreText->gameObject->transform->parent;
+		auto parent = child_to_keep->parent;
+		for(std::size_t i = 0; i < parent->childCount; i++) {
+			auto child = parent->GetChild(i);
+			if(child != child_to_keep)
+				objects.push_back(child->gameObject);
+		}
+	}
+	
+	// reactivate things in case they were deactivated
+	for(UnityEngine::GameObject *go : objects)
+		go->SetActive(true);
+	
+	// call original
+	ResultsViewController_SetDataToUI(self);
+
+	// modify menu in endless
+	if(endless::state.activated && self->_levelCompletionResults->levelEndStateType == GlobalNamespace::LevelCompletionResults::LevelEndStateType::Failed) {
+		// set score text
+		self->_scoreText->text = GlobalNamespace::ScoreFormatter::Format(endless::state.score+self->_levelCompletionResults->modifiedScore);
+
+		// activate/deactivate things
+		self->_scoreText->gameObject->transform->parent->parent->gameObject->SetActive(true);
+		self->_scoreText->gameObject->transform->parent->gameObject->SetActive(true);
+		self->_scoreText->gameObject->SetActive(true);
+		
+		for(UnityEngine::GameObject *go : objects)
+			go->SetActive(false);
+
+		// deactivate endless now that we're done
+		endless::state.activated = false;
+	}
+}
+
+
 
 namespace endless {
 	State state;
+	
+	// coroutine to update the time text
+	static custom_types::Helpers::Coroutine update_time_coroutine() {
+		while(true) {
+			if(endless::state.score_text) {
+				float elapsed = std::chrono::duration<float, std::milli>(std::chrono::steady_clock::now()-state.start_time).count()/1000.f;
+				int32_t seconds = static_cast<int32_t>(elapsed)%60;
+				int32_t minutes = static_cast<int32_t>(elapsed/60.f)%60;
+				// I wonder if anyone will actually ever get to hours
+				int32_t hours = static_cast<int32_t>(elapsed/3600.f);
+				std::stringstream stream;
+				stream << "Total Time ";
+				if(hours == 0) {
+					stream << minutes << ":" << std::setfill('0') << std::setw(2) << seconds;
+				} else {
+					stream << hours << ":" << std::setfill('0') << std::setw(2) << minutes << std::setfill('0') << std::setw(2) << seconds;
+				}
+				state.time_text->text = stream.str();
+			}
+			if(!state.activated)
+				co_return;
+			else
+				co_yield reinterpret_cast<System::Collections::IEnumerator *>(UnityEngine::WaitForSecondsRealtime::New_ctor(1));
+		}
+	}
 	
 	bool next_level(void) {
 		auto levelParams = get_next_level();
@@ -97,8 +211,27 @@ namespace endless {
 		if(!next_level())
 			return;
 		state.activated = true;
+		state.start_time = std::chrono::steady_clock::now();
+		state.score = 0;
+		// start timer
+		{
+			auto mth = UnityEngine::Object::FindObjectOfType<GlobalNamespace::MenuTransitionsHelper *>();
+			if(mth != nullptr)
+				mth->StartCoroutine(custom_types::Helpers::CoroutineHelper::New(update_time_coroutine()));
+			else
+				PaperLogger.warn("mth is null");
+		}
 		PaperLogger.info("Endless mode started!");
 	}	
+	
+	void set_score_text(int score) {
+		if(!endless::state.score_text)
+			return;
+		int total_score = score+endless::state.score;
+		std::string text = "Total Score ";
+		text += std::to_string(total_score);
+		endless::state.score_text->text = text;
+	}
 
 	std::optional<LevelParams> get_next_level() {
 		if(!SongCore::API::Loading::AreSongsLoaded())
@@ -173,7 +306,14 @@ namespace endless {
 		return LevelParams{level, characteristic, difficulty};
 	}
 	void register_hooks() {
-		INSTALL_HOOK(PaperLogger, pause_start_hook);
-		INSTALL_HOOK(PaperLogger, game_finish_hook);
+		INSTALL_HOOK(PaperLogger, PauseMenuManager_Start);
+		INSTALL_HOOK(PaperLogger, PauseMenuManager_MenuButtonPressed);
+
+		INSTALL_HOOK(PaperLogger, ScoreUIController_Start);
+		INSTALL_HOOK(PaperLogger, ScoreUIController_UpdateScore);
+
+		INSTALL_HOOK(PaperLogger, MenuTransitionsHelper_HandleMainGameSceneDidFinish);
+
+		INSTALL_HOOK(PaperLogger, ResultsViewController_SetDataToUI);
 	}
 }
